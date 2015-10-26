91af1eea81acf296a62fd2c9e5baabb4f522e772
Tobias Mielke <extern.tobias.mielke@audi.de>
2015-10-26 16:18:43 +0100

    Initial Check-In

---
+++ src/main/java/de/kayteem/lib/file/fileReader/IFileReader.java
@@ -0,0 +1,17 @@
+/**
+ * Created by:  VWTOBIA
+ * Created on:  24.06.2015
+ * Modified on: 09.09.2015
+ */
+
+package de.kayteem.lib.file.fileReader;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public interface IFileReader {
+
+    List<String> readLineStrings() throws IOException;
+
+}
