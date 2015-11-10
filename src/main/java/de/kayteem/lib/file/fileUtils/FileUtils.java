746d75dd4059985145f1bed47f0ce9066bbd5c1b
Tobias Mielke <extern.tobias.mielke@audi.de>
2015-11-10 12:59:37 +0100

    added method: retrieveJarFile

--- src/main/java/de/kayteem/lib/file/fileUtils/FileUtils.java
+++ src/main/java/de/kayteem/lib/file/fileUtils/FileUtils.java
@@ -1,7 +1,7 @@
 /**
  * Created by:  Tobias Mielke
  * Created on:  30.10.2015
- * Changed on:  30.10.2015
+ * Changed on:  10.11.2015
  */
 
 package de.kayteem.lib.file.fileUtils;
@@ -14,18 +14,22 @@ import java.security.CodeSource;
 
 public class FileUtils {
 
-    public static String retrieveExecutionPath(Class mainClass) throws FileNotFoundException {
-        String executionPath;
+    public static String retrieveExecutionPath(Class clazz) throws FileNotFoundException {
+        File file = retrieveJarFile(clazz);
+        return file.getParentFile().getPath() + "\\";
+    }
+
+    public static File retrieveJarFile(Class clazz) throws FileNotFoundException {
+        File file;
 
         try {
-            CodeSource codeSource = mainClass.getProtectionDomain().getCodeSource();
-            File file = new File(codeSource.getLocation().toURI().getPath());
-            executionPath = file.getParentFile().getPath() + "\\";
+            CodeSource codeSource = clazz.getProtectionDomain().getCodeSource();
+            file = new File(codeSource.getLocation().toURI().getPath());
         } catch (URISyntaxException e) {
             throw new FileNotFoundException(e.getMessage());
         }
 
-        return executionPath;
+        return file;
     }
 
 }
